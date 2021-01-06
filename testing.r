af1b4a2c72419e0b599bac3e4755ad8ba1a7095f
Zara Siddiqui <zara.nedian@gmail.com>
2021-01-07 03:43:56 +0500

    hghghj

--- testing.r
+++ testing.r
@@ -1,2 +1,10 @@
-#This is from GIT
-Print("ZAra Siddiqui")
+
+
+name= "zara"
+version = "RStudio in writing"
+print(name)
+print("these lines has been written in R studio")
+
+git config --global user.email "zara.nedian@gmail.com"
+git config --global user.name "zaRA SIDDIQUI"
+
